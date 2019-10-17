3f164d31ccde5373081cd4fd238231ff855c26a8
kk <kk@littlelights.ai>
2019-10-17 10:04:58 +0800

    request in get and post

--- examples/drumproxy.js
+++ examples/drumproxy.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const { DrumstickClient} =require("@kkito/drumstick");
+const client = new DrumstickClient({ host: process.env.DC_HOST, port: process.env.DC_PORT}, process.env.DC_KEY);
+
 var port = 8082;
 
 var Proxy = require('../');
@@ -11,25 +14,40 @@ proxy.onError(function(ctx, err, errorKind) {
   console.error(errorKind + ' on ' + url + ':', err);
 });
 
+
 proxy.onRequest(function(ctx, callback) {
-  console.log('=============== REQUEST: http://' + ctx.clientToProxyRequest.headers.host + ctx.clientToProxyRequest.url);
-  return callback();
+  console.log('onRequest!!!!')
+  ctx.clientToProxyRequest.resume();
+  if (ctx.proxyToServerRequestOptions.method === 'GET') {
+    let requestUrl = 'http://'
+    if (ctx.proxyToServerRequestOptions.port === 443) {
+      requestUrl = 'https://'
+    }
+    requestUrl = `${requestUrl}${ctx.proxyToServerRequestOptions.host}${ctx.proxyToServerRequestOptions.path}`
+    ctx.proxyToServerRequestOptions.method
+    ctx.proxyToServerRequestOptions.port
+    client.ensureRequest(requestUrl, ctx.proxyToServerRequestOptions.headers , 'binary', {})
+    .then(res => {
+      ctx.proxyToClientResponse.writeHead(res.status, res.headers)
+      ctx.proxyToClientResponse.write(res.body)
+    })
+    .catch(err => {
+      ctx.proxyToClientResponse.writeHead(502)
+    })
+  } else {
+    console.log(`other method found ${ctx.proxyToServerRequestOptions.method}`)
+    ctx.clientToProxyRequest.on('data' , (d) => {
+      console.log(d)
+      console.log('onData')
+      ctx.proxyToClientResponse.writeHead(200)
+      ctx.proxyToClientResponse.write(`params is ${d}`)
+      ctx.proxyToClientResponse.end()
+    })
+  }
+  callback();
 });
 
-proxy.onRequestData(function(ctx, chunk, callback) {
-  //console.log('request data length: ' + chunk.length);
-  return callback(null, chunk);
-});
 
-proxy.onResponse(function(ctx, callback) {
-  //console.log('RESPONSE: http://' + ctx.clientToProxyRequest.headers.host + ctx.clientToProxyRequest.url);
-  return callback(null);
-});
-
-proxy.onResponseData(function(ctx, chunk, callback) {
-  //console.log('response data length: ' + chunk.length);
-  return callback(null, chunk);
-});
 
 proxy.listen({ port: port });
 console.log('listening on ' + port);
