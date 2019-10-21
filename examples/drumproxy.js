024a763f3d37e42e7d03969313edba97d545f1ee
kk <kk@littlelights.ai>
2019-10-21 09:55:08 +0800

    proxy refactory

--- examples/drumproxy.js
+++ examples/drumproxy.js
@@ -1,51 +1,103 @@
 'use strict';
 
-const { DrumstickClient} =require("@kkito/drumstick");
-const client = new DrumstickClient({ host: process.env.DC_HOST, port: process.env.DC_PORT}, process.env.DC_KEY);
+const { DrumstickClient } = require("@kkito/drumstick");
+let client = new DrumstickClient({ host: process.env.DC_HOST, port: process.env.DC_PORT }, process.env.DC_KEY);
 
 var port = 8082;
 
 var Proxy = require('../');
 var proxy = Proxy();
 
-proxy.onError(function(ctx, err, errorKind) {
+let isRunning = false
+const paddingCtx = []
+
+proxy.onError(function (ctx, err, errorKind) {
   // ctx may be null
   var url = (ctx && ctx.clientToProxyRequest) ? ctx.clientToProxyRequest.url : '';
   console.error(errorKind + ' on ' + url + ':', err);
 });
 
 
-proxy.onRequest(function(ctx, callback) {
+proxy.onRequest(function (ctx, callback) {
   console.log('onRequest!!!!')
-  ctx.clientToProxyRequest.resume();
-  if (ctx.proxyToServerRequestOptions.method === 'GET') {
-    let requestUrl = 'http://'
-    if (ctx.proxyToServerRequestOptions.port === 443) {
-      requestUrl = 'https://'
-    }
-    requestUrl = `${requestUrl}${ctx.proxyToServerRequestOptions.host}${ctx.proxyToServerRequestOptions.path}`
-    ctx.proxyToServerRequestOptions.method
-    ctx.proxyToServerRequestOptions.port
-    client.ensureRequest(requestUrl, ctx.proxyToServerRequestOptions.headers , 'binary', {})
-    .then(res => {
-      ctx.proxyToClientResponse.writeHead(res.status, res.headers)
-      ctx.proxyToClientResponse.write(res.body)
-    })
-    .catch(err => {
-      ctx.proxyToClientResponse.writeHead(502)
-    })
+  if (isRunning) {
+    ctx.clientToProxyRequest.pause()
+    paddingCtx.push(ctx)
   } else {
-    console.log(`other method found ${ctx.proxyToServerRequestOptions.method}`)
-    ctx.clientToProxyRequest.on('data' , (d) => {
-      console.log(d)
-      console.log('onData')
-      ctx.proxyToClientResponse.writeHead(200)
-      ctx.proxyToClientResponse.write(`params is ${d}`)
-      ctx.proxyToClientResponse.end()
-    })
+    execCtx(ctx)
   }
   callback();
 });
+
+function execCtx(ctx) {
+
+  ctx.clientToProxyRequest.resume();
+  const contentLengthHeadher = ctx.proxyToServerRequestOptions.headers['content-length']
+  let contentLength = 0
+  if (!contentLengthHeadher) {
+    proxyRun(ctx, null)
+  } else {
+    contentLength = parseInt(contentLengthHeadher, 10)
+    let chunks = Buffer.alloc(0)
+
+    ctx.clientToProxyRequest.on('data', (d) => {
+      console.log(d)
+      console.log('onData')
+      chunks = Buffer.concat([chunks, d])
+      if (chunks.length >= contentLength) {
+        proxyRun(ctx, chunks)
+      }
+    })
+  }
+
+}
+
+function proxyRunFinish(ctx) {
+  isRunning = false
+  let nextCtx
+  while(nextCtx = paddingCtx.shift()) {
+    proxyRun(nextCtx)
+  }
+}
+
+function proxyRun(ctx , body) {
+  isRunning = true
+  let requestUrl = 'http://'
+  if (ctx.proxyToServerRequestOptions.port === 443) {
+    requestUrl = 'https://'
+  }
+  requestUrl = `${requestUrl}${ctx.proxyToServerRequestOptions.host}${ctx.proxyToServerRequestOptions.path}`
+
+  let myheader = ctx.proxyToServerRequestOptions.headers
+  delete myheader['content-length']
+
+  client = new DrumstickClient({ host: process.env.DC_HOST, port: process.env.DC_PORT }, process.env.DC_KEY);
+  console.log(`begin to request ${requestUrl}`)
+  return client.ensureRequestV2(
+      requestUrl,
+      ctx.proxyToServerRequestOptions.method,
+      myheader,
+      body, {}
+    )
+      // client.ensureRequest(requestUrl,ctx.proxyToServerRequestOptions.headers, 'binary', {body: chunks})
+      .then(res => {
+        console.log(`on response ${requestUrl}: ${res.status}`)
+        client.close()
+        if (res.headers['connection']) {
+          res.headers['connection'] = 'keep-alive, close'
+        }
+        ctx.proxyToClientResponse.writeHead(res.status, res.headers)
+        ctx.proxyToClientResponse.write(res.body)
+        ctx.proxyToClientResponse.end()
+        proxyRunFinish(ctx)
+      })
+      .catch(err => {
+        client.close()
+        console.log(`error happended ${err} ${typeof err}`)
+        ctx.proxyToClientResponse.writeHead(502)
+        proxyRunFinish(ctx)
+      })
+}
 
 
 
